92979f3be173e805882306c44eb1d6a8d81e8781
coder-gaia <alexandregaia911@gmail.com>
2025-10-30 18:40:16 -0300

    updating variables for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://nola-challenge.onrender.com/api",
   timeout: 30000,
 });
 
