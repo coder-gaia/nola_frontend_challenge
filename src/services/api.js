3a409c36a705fd611c3c2d2aeab458ba48225a03
coder-gaia <alexandregaia911@gmail.com>
2025-10-30 19:35:54 -0300

    imcreasing timeout

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL || "https://nola-challenge.onrender.com/api",
-  timeout: 30000,
+  timeout: 60000,
 });
 
 export default api;
